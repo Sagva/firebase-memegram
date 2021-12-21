327f29e5e4a9fdefdf60284f456e3151468c94a2
Elena Myadzeleva <sagva2014@gmail.com>
2021-12-21 11:40:33 +0100

    Added packages

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,32 +1,29 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+
 
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	apiKey: "AIzaSyDyxm9F6DTzivO_1jfuiU7QRX0Z5pE2Or0",
+	authDomain: "memegram-bef4c.firebaseapp.com",
+	projectId: "memegram-bef4c",
+	storageBucket: "memegram-bef4c.appspot.com",
+	messagingSenderId: "959213637186",
+	appId: "1:959213637186:web:a803070d66b201ac8967db",
 };
 
 // init firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
 // get firebase auth instance
-const auth = getAuth()
+const auth = getAuth();
 
 // get firebase firestore instance
-const db = getFirestore(app)
+const db = getFirestore(app);
 
 // get firebase storage instance
-const storage = getStorage(app)
+const storage = getStorage(app);
 
-export {
-	app as default,
-	auth,
-	db,
-	storage,
-}
+export { app as default, auth, db, storage };
